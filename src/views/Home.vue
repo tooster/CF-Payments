20b724ed1ecbe6f71db9582df1cb636ed7587035
Max De Vos <max@maxdevos.be>
2019-09-20 23:14:57 +0200

    Transaction list & detail ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Transactions</h1>
+    <div class="transaction-list">
+      <div v-for="transaction in transactions" :key="transaction.reference">
+        <router-link :to="{ name: 'payment-detail', params: {id: transaction.id}}">
+          <div class="transaction-list-item">
+            <h3>{{transaction.reference}}</h3>
+            {{transaction.amount}}€ |
+            {{transaction.created}} |
+            {{transaction.status}}
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      transactions: this.$store.state.transactions,
+    };
   },
 };
 </script>
+
+<style scoped lang="scss">
+  .transaction-list {
+    .transaction-list-item {
+      border: 1px solid black;
+      border-radius: 2px;
+      margin: 2px 0px;
+    }
+  }
+</style>
---
+++ src/views/PaymentDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1> Temp </h1>
+    <h3>Transaction {{this.$route.params.id}}</h3>
+    <p>amount: {{transaction.amount}}€</p>
+    <p>created: {{transaction.created}}</p>
+    <p>status: {{transaction.status}}</p>
+    <p>reference: {{transaction.reference}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    transaction() {
+      return this.$store.state.transactions.find(tran => tran.id === this.$route.params.id);
+    },
+  },
+};
+</script>
